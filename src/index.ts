14bd49b4352666d4dadbaf27541d76bed7521e15
Arjun gupta <rachnag352@gmail.com>
2025-09-17 23:24:04 +0530

    applied ts rules

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-"use strict";
 //TypeScript is a syntactic superset of JavaScript which adds static typing.
 // This basically means that TypeScript adds syntax on top of JavaScript, allowing developers to add types
 // let x: number = 1;
 // console.log(x);
-Object.defineProperty(exports, "__esModule", { value: true });
+
 // Types of data type in typescipt
 // there are 2 types of data type in ts:
 // primitive and non-primitive
+
 // Primitive data types:-
 // number,string,boolean,null,undifined,any
+
 // Q1. A simple code to take a username from the user and printing it
+
 /*let name: string = "Arjun";
 
 function greet(name: string) {
@@ -17,13 +19,16 @@ function greet(name: string) {
 }
 greet(name);
 */
+
 // Q2. write a fucntion that sums the 2 numbers
+
 /*function add(a: number, b: number) {
   let total: number = a + b;
   return total;
 }
 console.log(add(10, 20));
 */
+
 // Q.3 return false of the age is < 18
 /*
 function checkAge(age: number) {
@@ -35,11 +40,13 @@ function checkAge(age: number) {
 }
 console.log(checkAge(20));
  */
+
 // Q4. sending a fucntion as an argument to another function
-function getDelayed(fn) {
-    setTimeout(fn, 1000);
+
+function getDelayed(fn: (name: string) => void) {
+  setTimeout(fn, 1000);
 }
-getDelayed(() => {
-    console.log("Hello There");
-});
-//# sourceMappingURL=index.js.map
+function nameCall(name: string) {
+  console.log(`Hello ${name}`);
+}
+getDelayed(() => nameCall("Arjun"));
